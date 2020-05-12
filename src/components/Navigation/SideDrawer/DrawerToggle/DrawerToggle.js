39bacb3e6f571fd9e4ff38a5811eb2b182779868
Lavrici <razvan.lavric@gmail.com>
2020-05-12 17:06:26 +0300

    #5 - 151 - Adding a Hamburger Icon - an icon made by 3 styled dives

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import classes from './DrawerToggle.css'
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        {/* These dives do not need any styling because they will be styled correctly because he is styling all 
+        divs which are nested in a div or any element which has the DrawerToggle.css*/}
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawerToggle;
