d6c0cd20b036bd30715a9448a5035ad1c4fa3bc5
Lavrici <razvan.lavric@gmail.com>
2020-05-12 16:07:31 +0300

    #4 - Reusing backdrop for SideDrawer and closing SideDrawer when backdrop is clicked

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Fragment>
-        <Toolbar />
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Fragment>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDraweClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
 
-export default layout;
+    render() {
+        return (
+            <Fragment>
+                <Toolbar />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDraweClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        );
+    };
+};
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
 
 const sideDrawer = (props) => {
 
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
-            {/*This is an approach. Another very good aproach is to use a property here and in the Toolbar and set with inline -style the percentage  */}
-            <div className={classes.Logo}> {/* We have the .Logo class defined in 3 different .css files, but due to css MODULES, this is actually converted to three different css classes so that they do not interfere  */}
-                <Logo />
+        <Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                {/*This is an approach. Another very good aproach is to use a property here and in the Toolbar and set with inline -style the percentage  */}
+                <div className={classes.Logo}> {/* We have the .Logo class defined in 3 different .css files, but due to css MODULES, this is actually converted to three different css classes so that they do not interfere  */}
+                    <Logo />
+                </div>
+                {/* You can outsorce the nac to the NvigationItems just make sure to change the stying because now it is done in toolbar */}
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            {/* You can outsorce the nac to the NvigationItems just make sure to change the stying because now it is done in toolbar */}
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Fragment>
     );
 };
 
