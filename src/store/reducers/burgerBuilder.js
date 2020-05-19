ca24f8275d6129525fc4d397ad5229aa4c8065ac
Lavrici <razvan.lavric@gmail.com>
2020-05-19 22:15:10 +0300

    #41 - S17: Redux Advanced - 314 - Resetting the Price after Purchases

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -58,6 +58,7 @@ const ingredients = (state = initialState, action) => {
                     cheese: action.ingredients.cheese,
                     meat: action.ingredients.meat
                 },
+                totalPrice: 4,
                 error: false
             }
         case actionTypes.FETCH_INGREDIENT_FAILED:
