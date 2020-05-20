739324a13298c994d24182941dbffcbe2c8f0f46
Lavrici <razvan.lavric@gmail.com>
2020-05-20 20:37:23 +0300

    #47 - Authentication - 329 - Adding a Spinner - When we login or signup, we also shwo an error message if something is wrong

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css'
-
+import Spinner from '../../components/UI/Spinner/Spinner'
 import { connect } from 'react-redux'
 
 import * as actions from '../../store/actions/index'
@@ -94,7 +94,7 @@ class Auth extends Component {
             })
         }
 
-        const form = formEelementsArray.map(formElement => (
+        let form = formEelementsArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -106,8 +106,20 @@ class Auth extends Component {
                 changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))
 
+        if (this.props.loading) {
+            form = <Spinner />
+        }
+
+        let errorMessage = null;
+        if (this.props.error) {
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            )
+        }
+
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">{this.state.isSignUp ? 'SUBMIT' : 'SIGNIN'}</Button>
@@ -120,10 +132,17 @@ class Auth extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
     }
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
