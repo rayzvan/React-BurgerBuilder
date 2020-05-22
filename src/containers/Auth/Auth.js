be83ff7b0d8d04c7d78f511f5240cf984c60aa5c
Lavrici <razvan.lavric@gmail.com>
2020-05-22 22:06:22 +0300

    #57 - S19: Improvements - Sharing the Validation Method !!!!!

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -9,7 +9,7 @@ import { Redirect } from 'react-router-dom'
 
 import * as actions from '../../store/actions/index'
 
-import { updateObject } from '../../shared/utility'
+import { updateObject, checkValidity } from '../../shared/utility'
 
 class Auth extends Component {
     state = {
@@ -53,24 +53,6 @@ class Auth extends Component {
         }
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, controlName) => {
         //THIS IS HOW WE DID IT BEFORE USING UTILITY FUNCTION
         // const updatedControls = {
@@ -87,7 +69,7 @@ class Auth extends Component {
         const updatedControls = updateObject(this.state.controls, {
             [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
-                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
             })
         })
