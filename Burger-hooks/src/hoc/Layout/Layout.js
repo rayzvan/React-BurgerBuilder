45f8a74b5f0ee76cd002fc25e8a326800d3fa289
Lavrici <razvan.lavric@gmail.com>
2020-05-24 14:46:52 +0300

    #68 - S27: Hooks - 454 - Converting the Layout Component

--- Burger-hooks/src/hoc/Layout/Layout.js
+++ Burger-hooks/src/hoc/Layout/Layout.js
@@ -1,39 +1,38 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 import { connect } from 'react-redux'
 
-class Layout extends Component {
-    state = {
-        showSideDrawer: false
-    }
-    sideDraweClosedHandler = () => {
-        this.setState({ showSideDrawer: false })
+const layout = (props) => {
+
+    const [sideDrawerIsVisible, setSideDraweIsVisible] = useState(false);
+
+    const sideDrawerClosedHandler = () => {
+        setSideDraweIsVisible(false);
     }
 
-    sideDraweToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !prevState.showSideDrawer}
-        })
+    const sideDrawerToggleHandler = () => {
+        setSideDraweIsVisible(!sideDrawerIsVisible)
+        // this.setState((prevState) => {
+        //     return {showSideDrawer: !prevState.showSideDrawer}
+        // })
     }
 
-    render() {
-        return (
-            <Fragment>
-                <Toolbar
-                    isAuth={this.props.isAuthenticate}
-                    drawerToggleClicked={this.sideDraweToggleHandler}/>
-                <SideDrawer
-                    isAuth={this.props.isAuthenticate}
-                    open={this.state.showSideDrawer}
-                    closed={this.sideDraweClosedHandler} />
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Fragment>
-        );
-    };
+    return (
+        <Fragment>
+            <Toolbar
+                isAuth={props.isAuthenticate}
+                drawerToggleClicked={sideDrawerToggleHandler} />
+            <SideDrawer
+                isAuth={props.isAuthenticate}
+                open={sideDrawerIsVisible}
+                closed={sideDrawerClosedHandler} />
+            <main className={classes.Content}>
+                {props.children}
+            </main>
+        </Fragment>
+    );
 };
 
 const mapStateToProps = state => {
@@ -42,4 +41,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(Layout);
+export default connect(mapStateToProps)(layout);
