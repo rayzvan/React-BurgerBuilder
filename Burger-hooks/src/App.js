a14cb57d4c56401fe5686776558aa8a42f23e2ad
Lavrici <razvan.lavric@gmail.com>
2020-05-24 14:34:35 +0300

    #66 - S27: Hooks - Converting <App>

--- Burger-hooks/src/App.js
+++ Burger-hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
@@ -19,40 +19,39 @@ const asyncCheckout = asyncComponent(() => {
   return import('./containers/Checkout/Checkout');
 })
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoLogin();
-  }
+const app = (props) => {
+  
+  useEffect(() => {
+    props.onTryAutoLogin();
+  },[])
 
-  render() {
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path='/auth' component={asyncAuth} />
+      <Route path='/' component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
-        <Route path='/auth' component={asyncAuth} />
+        <Route path='/checkoutSummary' component={asyncCheckout} />
+        <Route path='/orders' component={asyncOrders} />
+        <Route path='/logout' component={Logout} />
         <Route path='/' component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
     );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path='/checkoutSummary' component={asyncCheckout} />
-          <Route path='/orders' component={asyncOrders} />
-          <Route path='/logout' component={Logout} />
-          <Route path='/' component={BurgerBuilder} />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
-
-    return (
-      <div>
-        <Layout>
-          {routes}
-        </Layout>
-      </div >
-    );
   }
+
+  return (
+    <div>
+      <Layout>
+        {routes}
+      </Layout>
+    </div >
+  );
 }
 
 const mapStateToProps = state => {
@@ -67,4 +66,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app));
